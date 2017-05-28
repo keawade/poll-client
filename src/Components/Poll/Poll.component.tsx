075e2e5ea50dbd653fff5bae7de7a18d041bc385
Keith Wade <keawade@gmail.com>
2017-05-28 13:56:41 -0500

    Viewable results

--- src/Components/Poll/Poll.component.tsx
+++ src/Components/Poll/Poll.component.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
 import './Poll.scss';
 
+import { Progress } from 'semantic-ui-react';
+
 interface IPollComponentProps {
   poll: IPoll;
 }
 
-const PollComponent: React.StatelessComponent<IPollComponentProps> = (props: IPollComponentProps) => (
-  <div>
-    <h2>{props.poll.question}</h2>
-    <ul>
+const PollComponent: React.StatelessComponent<IPollComponentProps> = (props: IPollComponentProps) => {
+  const total = props.poll.responses.length;
+  const results = props.poll.responseOptions.reduce((prev, curr) => ({ ...prev, [curr]: props.poll.responses.filter((res) => (res.response === curr)).length }), {});
+
+  return (
+    <div>
+      <h2>{props.poll.question}</h2>
       {props.poll.responseOptions.map((responseOption, j) => (
-        <li key={j}>{responseOption}</li>
+        <Progress
+          key={j}
+          value={results[responseOption]}
+          total={total}
+          >
+          {responseOption}
+        </Progress>
       ))}
-    </ul>
-  </div>
-);
+    </div>
+  );
+};
 
 export default PollComponent;
